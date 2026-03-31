a17988d19b361333066dadd9ffbd30a57c8be0b3
WolfyXD1234 <lwolfyw@gmail.com>
2026-03-31 20:36:34 +0000

    0.0000000001

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import random
+import pandas as pd
+import curses
 
+class tank:
+    def __init__(self):
+        self.hp = 10
+        self.damage = 5
+        self.damage_range = 1
+        self.hit_rate = 0.8
+
+tankA = tank()
+tankB = tank()
+turn = random.randint(0,1)
+while (tankA.hp > 0 or tankB.hp > 0):
+    print("temp")
+    break
